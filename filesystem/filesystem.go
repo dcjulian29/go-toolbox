3a0870d781fd10fa6fd102fd439461a1e462b981
Julian Easterling <julian@julianscorner.com>
2026-03-03 16:34:31 -0500

    extracted FileHash and ScanDirectory to separate files

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -16,25 +16,11 @@ limitations under the License.
 package filesystem
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"io"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 const FileModeExecutable = 0755
-
-func FileExists(path string) bool {
-	info, err := os.Stat(path)
-
-	if os.IsNotExist(err) {
-		return false
-	}
-
-	return !info.IsDir()
-}
 
 func DirectoryExists(path string) bool {
 	info, err := os.Stat(path)
@@ -71,6 +57,16 @@ func EnsureFileExist(path string, content []byte) error {
 	return nil
 }
 
+func FileExists(path string) bool {
+	info, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return !info.IsDir()
+}
+
 func RemoveDirectory(path string) error {
 	if DirectoryExists(path) {
 		files, err := filepath.Glob(filepath.Join(path, "*"))
@@ -97,60 +93,4 @@ func RemoveFile(path string) error {
 	}
 
 	return nil
-}
-
-func FileHash(path string) (string, error) {
-	hash := sha256.New()
-
-	sourceFile, err := os.Open(path)
-	if err != nil {
-		return "", err
-	}
-
-	defer sourceFile.Close() //nolint:errcheck
-
-	_, err = io.Copy(hash, sourceFile)
-
-	if err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(hash.Sum(nil)), nil
-}
-
-func ScanDirectory(path string, ignore []string) ([]string, []string, error) {
-	folders := []string{}
-	files := []string{}
-
-	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		_continue := false
-
-		for _, i := range ignore {
-			if strings.Contains(path, i) {
-				_continue = true
-			}
-		}
-
-		if !_continue {
-			m := f.Mode()
-
-			if m.IsDir() {
-				folders = append(folders, path)
-			} else if m.IsRegular() {
-				files = append(files, path)
-			}
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return []string{}, []string{}, err
-	}
-
-	return folders, files, nil
 }
