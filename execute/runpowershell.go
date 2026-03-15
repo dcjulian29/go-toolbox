68eff980b2c90383a35446c78e165a6d8323abf9
Julian Easterling <julian@julianscorner.com>
2026-03-14 21:50:28 -0400

    Added function to handle executing Powershell commands

---
+++ execute/runpowershell.go
@@ -0,0 +1,32 @@
+package execute
+
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"os/exec"
+)
+
+// RunPowershell executes a PowerShell command and streams stdout/stderr to the
+// caller's terminal.  An error is returned if the exit code is non-zero.
+func RunPowershell(command string) error {
+	cmd := exec.Command("powershell", "-NoProfile", "-NonInteractive",
+		"-ExecutionPolicy", "Bypass", "-Command", command)
+	cmd.Stderr = nil
+	cmd.Stdout = nil
+
+	return cmd.Run()
+}
