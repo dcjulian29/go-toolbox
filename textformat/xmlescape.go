45a60bb4c16dce327c4a5ffae1ffe5cce0ca7467
Julian Easterling <julian@julianscorner.com>
2026-03-15 16:02:21 -0400

    Added a function to escape XML strings

---
+++ textformat/xmlescape.go
@@ -0,0 +1,31 @@
+package textformat
+
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"strings"
+)
+
+// XMLEscape escapes the five predefined XML entities in s.
+func XMLEscape(s string) string {
+	s = strings.ReplaceAll(s, "&", "&amp;")
+	s = strings.ReplaceAll(s, "<", "&lt;")
+	s = strings.ReplaceAll(s, ">", "&gt;")
+	s = strings.ReplaceAll(s, `"`, "&quot;")
+	s = strings.ReplaceAll(s, "'", "&apos;")
+	return s
+}
