82d24f811e39e75398448bb61af0dfa116215fbf
Julian Easterling <julian@julianscorner.com>
2026-03-15 16:40:06 -0400

    Added Hyper-V virtual machine funtions

---
+++ hyperv/vm/disablesecureboot.go
@@ -0,0 +1,40 @@
+//go:build windows
+
+package vm
+
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"fmt"
+
+	"github.com/dcjulian29/go-toolbox/execute"
+	"github.com/dcjulian29/go-toolbox/textformat"
+)
+
+// DisableSecureBoot turns off Secure Boot for a Generation 2 VM.
+func DisableSecureBoot(name string) error {
+	script := fmt.Sprintf(
+		`Set-VMFirmware -VMName "%s" -EnableSecureBoot Off -ErrorAction Stop`,
+		textformat.EscapeForPowershell(name),
+	)
+
+	if err := execute.RunPowershell(script); err != nil {
+		return fmt.Errorf("disabling Secure Boot for VM %q: %w", name, err)
+	}
+
+	return nil
+}
---
+++ hyperv/vm/setdynamicmemory.go
@@ -0,0 +1,37 @@
+//go:build windows
+
+package vm
+
+/*
+Copyright © 2026 Julian Easterling
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"fmt"
+
+	"github.com/dcjulian29/go-toolbox/execute"
+	"github.com/dcjulian29/go-toolbox/textformat"
+)
+
+// SetDynamicMemory enables dynamic memory with the given startup/min/max values.
+func SetDynamicMemory(name string, startBytes, minBytes, maxBytes int64) error {
+	script := fmt.Sprintf(
+		`Set-VMMemory -VMName "%s" -DynamicMemoryEnabled $true `+
+			`-StartupBytes %d -MinimumBytes %d -MaximumBytes %d -ErrorAction Stop`,
+		textformat.EscapeForPowershell(name), startBytes, minBytes, maxBytes,
+	)
+
+	return execute.RunPowershell(script)
+}
